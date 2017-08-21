bf24844bc6047d001c28d99ed976b168b8986b36
Danny AJ <linshengte@gmail.com>
2017-08-21 10:18:56 +0800

    Add files via upload

---
+++ install_scripts/install_kubelet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR=$( cd "$(dirname "$0")" && pwd )
+
+source ${DIR}/../config
+
+cat <<EOF | sudo tee /etc/systemd/system/kubelet.service
+[Unit]
+Description=Kubernetes Kubelet
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStart=/opt/kubernetes/server/bin/kubelet \\
+--hostname-override=$(hostname -s) \\
+--api-servers=http://${MASTER_LB_IP}:8080 \\
+--logtostderr=true \\
+--cloud-config=/opt/kubernetes/config/cloud.conf \\
+--cloud-provider=openstack
+Restart=on-failure
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable kubelet
+systemctl restart kubelet
+
